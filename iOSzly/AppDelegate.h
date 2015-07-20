8f56db58ab9f7189b56be421b2f31c360ae3a403
zly <zly@zlydeMacBook-Pro.local>
2015-07-20 10:36:41 +0800

    Initial Commit

---
+++ iOSzly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSzly
+//
+//  Created by zly on 15/7/20.
+//  Copyright (c) 2015年 zly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
